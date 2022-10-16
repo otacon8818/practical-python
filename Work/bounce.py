a340e82fbd730090d1bf2a5d750784c5d06bc691
Richard Wang <wanglu8818@gmail.com>
2022-10-16 21:17:38 +1030

    mod Work/bounce.py

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,13 @@
 # bounce.py
 #
 # Exercise 1.5
+height = 100  # 100 meters
+# bounce = 0
+bounce = 1
+# times = 1  # initial time
+# * not need variable : times
+while bounce <= 10:
+    height = height * (3 / 5)  # * height is dynamical change
+    print(bounce, round(height, 4))
+    bounce += 1
+    # height = bounce
